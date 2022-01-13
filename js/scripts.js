f205b579c0398008371512f90ea8323cb9186b2a
DavideVin01 <davidevinciguerradv01@gmail.com>
2022-01-13 18:09:42 +0100

    Completes 1st Snack

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,26 @@
+// # Snack 1
 // Il software deve chiedere per 5 volte all’utente di inserire un numero.
 // Il programma stampa la somma di tutti i numeri inseriti.
 // Esegui questo programma in due versioni, con il for e con il while.
+
+let sum = 0;
+
+// # Ciclo For
+// for (let i = 1; i <= 5; i++) {
+//     const number = parseInt(prompt('Scrivi un numero.').trim());
+//     console.log(`Numero digitato: ${number} - (${i}/5)`);
+//     sum += number;
+// }
+
+// Ciclo While
+let i = 1;
+while (i <= 5) {
+    const number = parseInt(prompt('Scrivi un numero.').trim());
+    console.log(`Numero digitato: ${number} - (${i}/5)`);
+    sum += number;
+
+    i++
+}
+
+// Somma
+console.log('Il risultato è', sum);
